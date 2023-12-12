532ebc5efbcc257572f3aa5a3f89fa5942a57fd8
carlomarcon <carlomarcon1998@gmail.com>
2023-12-12 15:41:22 +0100

    ADD: table

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
 <?php
@@ -50,11 +51,34 @@ $hotels = [
 
 ];
 
-var_dump($hotels)
+// var_dump($hotels)
 
 ?>
 
 
-<h1><?php echo $hotels[3]['name'] ?></h1>
+
+
+<table class="table table-bordered border-primary">
+    <thead>
+      <th>Nome</th>
+      <th>DESCRIZIONE</th>
+      <th>PARCHEGGIO</th>
+      <th>VOTO</th>
+      <th>DISTANZA DAL CENTRO</th>
+    </thead>
+<?php 
+foreach ($hotels as $item) { ?>
+<tr class="table-danger">
+<td class="table-danger"><?php echo $item['name'] ?></td>
+<td class="table-danger"><?php echo $item['description'] ?></td>
+<td class="table-danger"><?php if($item['parking']==true){ echo 'Si';} else{ echo 'No';}?> </td>
+<td class="table-danger"><?php echo $item['vote']  ?></td>
+<td class="table-danger"><?php echo $item['distance_to_center'] ?> Km</td>
+</tr>
+
+<?php } ?>
+</table>
+
+
 </body>
 </html>
